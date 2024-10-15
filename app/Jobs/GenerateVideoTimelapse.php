2d8fa0d16d1682f0a1661042b8300e78423565db
PhamDat <tiendathb94@gmail.com>
2024-10-15 08:32:00 +0700

    update

--- app/Jobs/GenerateVideoTimelapse.php
+++ app/Jobs/GenerateVideoTimelapse.php
@@ -39,11 +39,10 @@ class GenerateVideoTimelapse implements ShouldQueue
         Log::info($ffmpeg_cmd);
         Log::info('---------------------------');
 
-        RequestVideoTimelapse::query()->where('code', $this->path)->update(['is_handled' => 1]);
         exec($ffmpeg_cmd);
 
         RequestVideoTimelapse::query()->where('code', $this->path)->update(['is_handled' => 1]);
-        
+
         // put s3 and send mail
     }
 }
