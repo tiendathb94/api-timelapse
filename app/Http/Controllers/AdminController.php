33356cab1429740a4b4ecae7957e1fa6dfca0c84
PhamDat <tiendathb94@gmail.com>
2024-10-21 14:20:16 +0700

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -58,6 +58,7 @@ class AdminController extends Controller
     public function getUser()
     {
         $user = Auth::user();
+        $user->load(['roles']);
         return response()->json(['data' => $user]);
     }
 
